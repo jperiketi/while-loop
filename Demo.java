63f36ea8892c6e74e31690a8b7a6779a9006039d
Jagadeesh <90107601+jperiketi@users.noreply.github.com>
2024-02-04 21:44:37 -0600

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo
+{
+    public static void main(String a[])
+    {
+        int i =1;
+        while(true){
+            System.out.println("Hi" + 1);
+            i++;
+        }
+    }
+}javac
